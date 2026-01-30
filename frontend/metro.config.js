abe06964e52bd4dfd468b8a19e49abc3ddf8b2b7
Alaoscorty <133812869+Alaoscorty@users.noreply.github.com>
2026-01-30 17:56:47 +0100

    nouvealle mise à jour

--- frontend/metro.config.js
+++ frontend/metro.config.js
@@ -1,10 +1,25 @@
-const { getDefaultConfig } = require('expo/metro-config');
+const { getDefaultConfig } = require("expo/metro-config");
+const path = require("path");
 
 const config = getDefaultConfig(__dirname);
 
-// 🔥 FORCER axios browser (CRITIQUE)
-config.resolver.alias = {
-  axios: require.resolve('axios/dist/browser/axios.cjs'),
+// 🔥 FORCER axios vers le build browser
+config.resolver.extraNodeModules = {
+  ...(config.resolver.extraNodeModules || {}),
+
+  // axios
+  axios: path.resolve(__dirname, "node_modules/axios/dist/browser/axios.cjs"),
+
+  // polyfills node
+  crypto: require.resolve("crypto-browserify"),
+  stream: require.resolve("stream-browserify"),
+  buffer: require.resolve("buffer"),
+  assert: require.resolve("assert"),
+  util: require.resolve("util/"),
+  url: require.resolve("url/"),
+  http: require.resolve("stream-http"),
+  https: require.resolve("https-browserify"),
+  zlib: require.resolve("browserify-zlib"),
 };
 
 module.exports = config;
